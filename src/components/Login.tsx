b7c3d66d3a667f85684324fc879751e72fb42490
José Enrique <otipip90@gmail.com>
2021-12-20 01:41:03 -0500

    add style to login navbar

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,8 +4,22 @@ type IProps = {
   props?: any;
 };
 
-export const Login = ({ props }: IProps) => (
-  <>
-    <h1>This is a login components...</h1>
-  </>
-);
+export const Login = ({ props }: IProps) => {
+  return (
+    <>
+      <div className='ml-2 py-2 flex justify-between flex-nowrap relative content-center'>
+        <a href='/' className='w-40 h-5'>
+          <img src='/images/login-logo.svg' alt='' />
+        </a>
+        <div className='flex'>
+          <div className='text-xl px-2.5 py-3 font-sans text-gray-500 cursor-pointer hover:bg-zinc-200 hover:rounded-md hover:text-black'>
+            Join now
+          </div>
+          <div className='border-rose-900 px-2.5 py-3 mr-4 text-xl text-blue-600 '>
+            Sign In
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
