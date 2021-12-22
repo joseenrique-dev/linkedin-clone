4a2c7cd26fd84317bac20257d533a061b275675f
José Enrique <otipip90@gmail.com>
2021-12-21 22:48:02 -0500

    created initial structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='w-full bg-white z-50'>
+      <div className='py-2.5 border border-b-gray-300'>
+        <img src='/images/home-logo.svg' alt='' />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
