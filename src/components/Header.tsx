f49df5c35b7e91abbd63650a80dc016674d8ed60
José Enrique <otipip90@gmail.com>
2021-12-26 01:08:21 -0500

    working on elements icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
   return (
     <div className='w-full bg-white z-50'>
-      <div className='border border-b-gray-300 flex'>
-        <div className='pl-5'>
+      <div className='border border-b-gray-300 flex grow'>
+        <div className='pl-24 py-1.5'>
           <a href='/home'>
             <img src='/images/home-logo.svg' alt='' />
           </a>
@@ -12,11 +12,11 @@ const Header = () => {
         <div className='relative'>
           <label className='relative block pl-2'>
             <span className='sr-only'>Search</span>
-            <span className='absolute inset-y-0 left-0 flex items-center pl-2'>
+            <span className='absolute inset-y-0 left-0 flex items-center pl-2 py-1.5'>
               <svg className='h-5 w-5 fill-gray-300' viewBox='0 0 20 20'></svg>
             </span>
             <input
-              className='placeholder:text-gray-400 block bg-gray-200 w-full border rounded-sd py-1.5 pl-9 pr-3 shadow-sm focus:outline-none focus:border-gray-300 focus:ring-gray-300 focus:border focus:w-96 focus:ring-1 sm:text-sm'
+              className='placeholder:text-gray-400 block bg-gray-200 w-full border rounded-sd py-1.5 pl-9 pr-3 shadow-sm focus:outline-none focus:border-gray-300 focus:ring-gray-300 focus:border focus:w-80 focus:ring-1 sm:text-sm'
               placeholder='Search'
               type='text'
               name='search'
@@ -24,6 +24,19 @@ const Header = () => {
           </label>
           <div className='absolute top-2.5 left-5'>
             <img src='/images/search-icon.svg' alt='' />
+          </div>
+        </div>
+        <div className='flex items-center'>
+          <div>
+            <a
+              className='flex flex-col no-underline items-center leading-normal bg-transparent justify-center font-normal text-xs min-w-min min-h-min'
+              href='/'
+            >
+              <img src='/images/nav-home.svg' alt='' />
+              <span className='text-normalTextHeader hover:text-focusTextHeader active:text-focusTextHeader'>
+                Home
+              </span>
+            </a>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        normalTextHeader: 'rgba(0,0,0,0.6)',
+        focusTextHeader: 'rgba(0,0,0,0.9)',
+      },
+      minHeight: {
+        3.25: '3.25rem',
+      },
+    },
   },
   plugins: [],
 };
