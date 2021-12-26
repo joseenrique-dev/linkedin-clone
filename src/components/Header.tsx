77127684e34cab5379b1945029d720bb9647ade1
José Enrique <otipip90@gmail.com>
2021-12-26 00:15:10 -0500

    better code in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,19 +10,19 @@ const Header = () => {
           </a>
         </div>
         <div className='relative'>
-          <label className='relative block'>
+          <label className='relative block pl-2'>
             <span className='sr-only'>Search</span>
             <span className='absolute inset-y-0 left-0 flex items-center pl-2'>
               <svg className='h-5 w-5 fill-gray-300' viewBox='0 0 20 20'></svg>
             </span>
             <input
-              className='placeholder:text-gray-400 block bg-gray-200 w-full border rounded-md py-1.5 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
+              className='placeholder:text-gray-400 block bg-gray-200 w-full border rounded-sd py-1.5 pl-9 pr-3 shadow-sm focus:outline-none focus:border-gray-300 focus:ring-gray-300 focus:border focus:w-96 focus:ring-1 sm:text-sm'
               placeholder='Search'
               type='text'
               name='search'
             />
           </label>
-          <div className='absolute top-3 left-3'>
+          <div className='absolute top-2.5 left-5'>
             <img src='/images/search-icon.svg' alt='' />
           </div>
         </div>
