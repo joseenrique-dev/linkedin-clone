d8e86dec21880accf71631f735d3ce9a51cb4c24
José Enrique <otipip90@gmail.com>
2022-01-02 23:14:43 -0500

    added configuration to redux browser plugin.

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,7 +1,11 @@
-import { applyMiddleware, createStore } from 'redux';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
