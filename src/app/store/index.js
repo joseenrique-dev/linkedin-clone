48859d1067c49e46a997d2ae4360a56218885489
José Enrique <otipip90@gmail.com>
2022-01-02 21:09:52 -0500

    added redux-thunk (reverted from commit 5bef08eadc0df69890ddd659f833698eebd4e640)

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,7 +1,6 @@
-import { applyMiddleware, createStore } from 'redux';
+import { createStore } from 'redux';
 import rootReducer from '../reducers';
-import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, {});
 
 export default store;
